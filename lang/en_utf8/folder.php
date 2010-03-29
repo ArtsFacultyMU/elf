159f32bcf3cb7fadad5b7a62ed2ffe56a4b100c1
David Mudrak <david.mudrak@gmail.com>
2010-03-29 20:48:42 +0000

    MDL-21694 Moving Folder strings

---
+++ lang/en_utf8/folder.php
@@ -0,0 +1,8 @@
+<?php
+
+$string['contentheader'] = 'Content';
+$string['foldercontent'] = 'Files and subfolders';
+$string['folderadministration'] = 'Folder administration';
+$string['modulename'] = 'Folder';
+$string['modulenameplural'] = 'Folders';
+$string['neverseen'] = 'Never seen';
