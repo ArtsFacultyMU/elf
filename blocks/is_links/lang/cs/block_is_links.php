8ccb8bdbaf3fb6664b3e02332ac34d375cfce7f0
Richard Holaj <holajr22@gmail.com>
2017-04-10 13:29:59 +0200

    Added is_links blocks

---
+++ blocks/is_links/lang/cs/block_is_links.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://elf.phil.muni.cz/moodledev/moodle2
+ *
+ * @package    block
+ * @subpackage is_links
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['examsignup'] = 'Přihlašování na zkoušky';
+$string['faculty'] = 'Fakulta';
+$string['iscodes'] = 'Kódy podle IS MU';
+$string['mainpage'] = 'Záznamník učitele';
+$string['marks'] = 'Hodnocení studentů';
+$string['missingconfiguration'] = 'Je nutné nastavit parametry bloku.';
+$string['resources'] = 'Studijní materiály';
+$string['semester'] = 'Semestr';
+$string['settings_header'] = 'Nastavení';
+$string['shortsylabus'] = 'Informace o předmětu';
+$string['studentslist'] = 'Seznam studentů';
