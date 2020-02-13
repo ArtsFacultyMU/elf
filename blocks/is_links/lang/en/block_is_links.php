62c1d86a3c3d1d7b82006494f4c4fa912b7a5b87
Vojtěch Mrkývka <vojtech.mrkyvka@gmail.com>
2020-02-13 15:58:42 +0100

    Added blocks/is_links (custom plugin)

---
+++ blocks/is_links/lang/en/block_is_links.php
@@ -0,0 +1,45 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://elf.phil.muni.cz/moodledev/moodle2
+ *
+ * @package    block
+ * @subpackage is_links
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['examsignup'] = 'Examination Dates';
+$string['iscodes'] = 'IS Codes';
+$string['mainpage'] = 'Teacher\'s Notebook';
+$string['marks'] = 'Evaluation Form';
+$string['missingconfiguration'] = 'Missing block configuration.';
+$string['resources'] = 'Study materials';
+$string['shortsylabus'] = 'Course Information';
+$string['studentslist'] = 'List of Students';
+$string['exams'] = 'Exams';
+$string['interactivesylabus'] = 'Interactive Sylabus';
+$string['ismu'] = 'IS MU';
+$string['faculty'] = 'Faculty';
+$string['pluginname'] = 'Links to IS MU';
+$string['semester'] = 'Semester';
+$string['settings_header'] = 'Settings';
+$string['is_links:student_view'] = 'Student View';
+$string['is_links:teacher_view'] = 'Teacher View';
+$string['is_links:addinstance'] = 'Add new IS Links block';
