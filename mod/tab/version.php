716c689908316392f04636dbc3a7c1e5b6372721
Vojtěch Mrkývka <vojtech.mrkyvka@gmail.com>
2022-06-03 00:02:05 +0200

    Added plugin mod_tab

---
+++ mod/tab/version.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * *************************************************************************
+ * *                         OOHOO - Tab Display                          **
+ * *************************************************************************
+ * @package     mod                                                       **
+ * @subpackage  tab                                                       **
+ * @name        tab                                                       **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+$plugin->version = 2019062400;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2019052000;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '3.0.0 (Build: 2019062400)';
+$plugin->cron = 0;           // Period for cron to check this module (secs)
+$plugin->component = 'mod_tab';
