14a29705a348c77ed50e71abeebdd548da2b4ada
Vojtěch Mrkývka <vojtech.mrkyvka@gmail.com>
2023-04-03 10:53:51 +0200

    Added plugin tinymce_helixmedia

---
+++ lib/editor/tinymce/plugins/helixmedia/lang/en/tinymce_helixmedia.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for Moodle MEDIAL plugin.
+ *
+ * @package   tinymce_helixmedia
+ * @copyright 2013 Streaming LTD
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Insert MEDIAL Resource';
+
+/* This plugin abuses strings from the standard TinyMCE advimage plugin, there is no need to duplicate them here. */
+$string['helixmedia:browsemedia'] = 'Find or upload a resources in the Media Library...';
+$string['helixmedia:add_media'] = 'Add a video or audio resource from the Media Library.';
